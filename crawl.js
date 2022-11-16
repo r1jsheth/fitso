2bf6a52f54ea7dcf752812a3ae7fdc1cb60404f4
Raj Sheth <rajpsheth@gmail.com>
2022-11-16 14:14:16 +0530

    Format and add using prettier

--- crawl.js
+++ crawl.js
@@ -1,27 +1,33 @@
-const url = "https://api.getfitso.com/v1/web/getPageData?path=/bangalore/fitso-koramangala-pk-sports/badminton/slots"
-import fetch from 'node-fetch'
-globalThis.fetch = fetch
-
+const url =
+  "https://api.getfitso.com/v1/web/getPageData?path=/bangalore/fitso-koramangala-pk-sports/badminton/slots";
+import fetch from "node-fetch";
 
 fetch(url)
-  .then(x => x.json())
-  .then(y => {
-    const badmintonSlots = y['pageData']['slotSections'];
+  .then((x) => x.json())
+  .then((y) => {
+    const badmintonSlots = y["pageData"]["slotSections"];
     console.log(badmintonSlots);
     const todaysSlot = badmintonSlots[0];
     const tomorrowsSlot = badmintonSlots[1];
-    badmintonSlots.forEach(slotDay => {
+    badmintonSlots.forEach((slotDay) => {
       // console.log({slotDay});
-      slotDay['sections'].forEach(section => {
+      slotDay["sections"].forEach((section) => {
         // console.log({section});
-        section['slots'].forEach(slot => {
+        section["slots"].forEach((slot) => {
           // console.log({slot});
-          if ('isDisabled' in slot)
-          {
+          if ("isDisabled" in slot) {
             // console.log("ohhh no this slot is booked ---> ", slot['title']);
-          } else
-          {
-            console.log("yess this slot can be booked ---> ", slotDay['title'], ', ', slotDay['subtitle'], ', ', section['title'], ', ', slot['title']);
+          } else {
+            console.log(
+              "yess this slot can be booked ---> ",
+              slotDay["title"],
+              ", ",
+              slotDay["subtitle"],
+              ", ",
+              section["title"],
+              ", ",
+              slot["title"]
+            );
           }
         });
       });
