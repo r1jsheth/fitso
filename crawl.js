db5a96c0832bdaf345b77b34c2a4915758185270
Raj Sheth <rajpsheth@gmail.com>
2022-12-01 10:52:49 +0530

    Minor changes

--- crawl.js
+++ crawl.js
@@ -1,35 +1,41 @@
-const url =
-  "https://api.getfitso.com/v1/web/getPageData?path=/bangalore/fitso-koramangala-pk-sports/badminton/slots";
 import fetch from "node-fetch";
+// import y from './sample-slot-data.json' assert { type: "json" };
+
+const facilityCenter = "fitso-koramangala-pk-sports";
+const sport = "badminton";
+
+const url = `https://api.getfitso.com/v1/web/getPageData?path=/bangalore/${facilityCenter}/${sport}/slots`;
+
+const SEVEN_PM_SLOT_TIME = "7- 7:50 PM";
+const EIGHT_PM_SLOT_TIME = "8- 8:50 PM";
+const NINE_PM_SLOT_TIME = "9- 9:50 PM";
 
 fetch(url)
   .then((x) => x.json())
   .then((y) => {
-    const badmintonSlots = y["pageData"]["slotSections"];
-    console.log(badmintonSlots);
-    const todaysSlot = badmintonSlots[0];
-    const tomorrowsSlot = badmintonSlots[1];
-    badmintonSlots.forEach((slotDay) => {
-      // console.log({slotDay});
-      slotDay["sections"].forEach((section) => {
-        // console.log({section});
-        section["slots"].forEach((slot) => {
-          // console.log({slot});
-          if ("isDisabled" in slot) {
-            // console.log("ohhh no this slot is booked ---> ", slot['title']);
-          } else {
+  const sportSlotResult = y["pageData"]["slotSections"];
+  sportSlotResult.forEach((slotDay) => {
+    slotDay["sections"].forEach((section) => {
+      section["slots"].forEach((slot) => {
+        if (!("isDisabled" in slot)) {
+          if (
+            [
+              SEVEN_PM_SLOT_TIME,
+              EIGHT_PM_SLOT_TIME,
+              NINE_PM_SLOT_TIME,
+            ].includes(slot["title"])
+          ) {
             console.log(
-              "yess this slot can be booked ---> ",
+              "Available::",
               slotDay["title"],
-              ", ",
               slotDay["subtitle"],
-              ", ",
               section["title"],
-              ", ",
-              slot["title"]
+              slot["title"],
             );
           }
-        });
+        } else {
+        }
       });
     });
   });
+// });
